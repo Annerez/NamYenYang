38981f41814cd1844bc0125fc6f65a50dff6110f
Anner <98201562+Annerez@users.noreply.github.com>
2023-06-24 17:17:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 const firebaseConfig = {
     apiKey: "AIzaSyCmMz2W--JMKOgdw9cs-QPHNmgXoNgPyXc",
     authDomain: "namyenyang.firebaseapp.com",
@@ -28,10 +28,6 @@ element.innerHTML = data;
 });
 }
 
-function formatData(data, unit) {
-return data + ' ' + unit;
-}
-
 for (const key in refs) {
 onValue(refs[key], (snapshot) => {
 const data = snapshot.val();
@@ -43,3 +39,24 @@ if (key === 'temp') {
 element.innerHTML = formatData(data, unit);
 });
 }
+
+function formatData(data, unit) {
+return data + ' ' + unit;
+}
+
+
+const checkButton = document.getElementById('check');
+const showButton = document.getElementById('show');
+const hot = document.getElementById('yeng').innerHTML;
+const body = document.getElementById('body');
+
+checkButton.addEventListener('click', function() {
+    if (hot == "Yes") {
+        body.classList.toggle('bg-sky-400');
+    }
+    if (hot == "No") {
+        body.classList.toggle('bg-red-400');
+    }
+  checkButton.classList.toggle('hidden');
+  showButton.classList.toggle('hidden');
+});
